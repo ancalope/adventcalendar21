e2b5255263f88a213851121852ef3a6c87ef859b
ancalope <ancalope90@gmail.com>
2021-12-02 21:10:35 +0100

    Añadidos los problemas del día 2

---
+++ day2/problema02.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+var input = fs.readFileSync('input.txt', 'utf-8').split('\r\n');
+function movement(input) {
+    var horizontal = 0;
+    var depth = 0;
+    var aim = 0;
+    input.forEach(command => {
+        var dir = command.split(' ')[0];
+        var val = parseInt(command.split(' ')[1]);
+        switch (dir) {
+            case 'forward':
+                horizontal += val;
+                depth += aim * val;
+                break;
+            case 'up':
+                aim -= val;
+                break;
+            case 'down':
+                aim += val;
+                break;
+            default:
+                break;
+        }
+    });
+    return horizontal * depth;
+}
+
+var result = movement(input);
+console.log(result);
